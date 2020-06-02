dbf0252151aae304684db5ed19395b0468bcb8e3
huyct1996 <39883328+huyct1996@users.noreply.github.com>
2020-06-02 23:30:46 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ Drop.prototype.fall = function() {
 Drop.prototype.show = function() {
     let thick = map(this.z, 0, 20, 1, 3);
     strokeWeight(thick);
-    stroke(98, 71, 247);
+    stroke(218, 22, 22); //98, 71, 247
     line(this.x, this.y, this.x, this.y + this.leng);
 };
 
