6d211b7a2f80651169d9291d564445f60d700360
huyct1996 <39883328+huyct1996@users.noreply.github.com>
2020-06-03 18:50:26 +0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,7 +24,7 @@ Drop.prototype.show = function() {
 var d = new Array(500);
 
 function setup() {
-    createCanvas(width, height);
+    createCanvas(displayWidth, displayHeight);
     for(let i = 0; i < d.length; i++) {
         d[i] = new Drop();
     }
